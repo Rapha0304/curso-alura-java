53a35dfa85bb6e8e6fd716e2b659aa12ad826321
raphaelaramos137@gmail.com <92964435+Rapha0304@users.noreply.github.com>
2023-10-08 15:30:41 -0300

    first commit

---
+++ src/TestaCondicional.java
@@ -0,0 +1,20 @@
+
+public class TestaCondicional {
+		
+	public static void main(String[] args) {
+		System.out.println("testanto condicionais");
+		int idade = 16;
+		int quantidadePessoas = 3;
+		
+		if (idade >= 18) {
+			System.out.println("voce tem mais de 18 anos");
+			System.out.println("seja bem vindo");
+		}
+		else {
+			if(quantidadePessoas >=2) {
+			System.out.println("infelizmente voce não pode entrar," + "pois esta acompanhado");
+		
+			}
+		}	
+	}
+}
---
+++ src/TestaEscopo.java
@@ -0,0 +1,27 @@
+
+public class TestaEscopo {
+	public static void main(String[] args) {
+		System.out.println("testanto condicionais");
+		
+		
+		int idade = 20;
+		int quantidadePessoas = 3;
+		
+		boolean acompanhado;
+		
+		if(quantidadePessoas >=2) {
+			acompanhado = true;
+			} else {
+			 acompanhado = false;
+			}
+		
+		System.out.println("valor de acompanhado = " + acompanhado);
+		if (idade >= 18 && acompanhado) {
+			System.out.println("seja bem vindo");
+		} else {
+				System.out.println("infelizmente voce nao pode entrar");		
+
+			}
+		}
+	}
+
